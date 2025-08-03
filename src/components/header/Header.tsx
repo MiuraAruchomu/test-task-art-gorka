d73a54aac241f305b96e53ac9188fe9efd1c5e8b
MiuraAruchomu <76885969+MiuraAruchomu@users.noreply.github.com>
2025-08-03 16:16:08 +0300

    fixed header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,12 +7,27 @@ import HeaderLogo from './Logo';
 import HeaderDesktopMenu from './DesktopMenu';
 import HeaderButtons from './HeaderButtons';
 import HeaderBurgerMenu from './BurgerMenu';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const isDropDownMenuopen = useAppSelector(selectIsDropdownMenuOpen);
 
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className={`${styles.header} container`}>
+    <header
+      className={`${styles.header} ${isScrolled ? styles.scrolled : ''} ${isDropDownMenuopen ? styles['menu-is-open'] : ''} container`}
+    >
       <HeaderLogo />
       <HeaderDesktopMenu />
       <HeaderButtons />
